19a93a9779c41c191193c78d132c7f30bf849a55
Renata Guidali <reguidali@hotmail.com>
2021-05-08 21:24:34 -0300

    quem é esse pokemon?

--- src/components/pokemon/index.js
+++ src/components/pokemon/index.js
@@ -2,11 +2,19 @@ import { useState,useEffect } from 'react';
 
 function Pokemon() {
     
+    const [pokemonName, setPokemonName] = useState("");
+
     const [state, setState] = useState({});
+
+    const onChange = (event) => {
+        const { value } = event.target
+        setPokemonName(value); 
+        console.log(value);
+     };
 
     const getPokemon = async () => { //async = determina q é assíncrono
 
-        const result = await fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((response) => { //await = fala que é pra esperar o retorno
+        const result = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName).then((response) => { //await = fala que é pra esperar o retorno
             console.log(response);
             return response.json();
         }).then((response) => {
@@ -18,13 +26,19 @@ function Pokemon() {
 
     useEffect( () => { //só vai atualizar quando tiver alteração
         getPokemon();
-    },[]);
+    },[onChange]);
 
     return (
-      <h1>
-        Ola pokemon.
-        {state.abilities ? state.abilities[1]?.ability?.name : null} {/*? = só imprima se existir*/}
-      </h1>
+    <header>
+        <div className='searchPokemon'>
+          WHO IS THIS POKEMON?  <input type="text" onChange={onChange} />
+        </div>
+        <div className='cardPokemon'>
+            <img alt='Pokemonito' src={state.sprites ? state.sprites?.front_default : null}/>
+            <p>Name: {state.species ? state.species?.name : null} {/*? = só imprima se existir*/}</p>
+            <p>Ability: {state.abilities ? state.abilities[1]?.ability?.name : null} {/*? = só imprima se existir*/}</p>
+        </div>
+    </header>
     );
   }
   
